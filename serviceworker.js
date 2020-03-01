d4f382f605c32fe702e0207c6cbfeeff648719d4
csharpfiddle <60544881+csharpfiddle@users.noreply.github.com>
2020-03-01 11:48:30 +0600

    Fix status bar for iPhone

--- serviceworker.js
+++ serviceworker.js
@@ -1,11 +1,8 @@
-﻿console.log("This is service worker talking!");
-var cacheName = 'csharp-fiddle';
+﻿var cacheName = 'csharp-fiddle';
 
 self.addEventListener('install', function (e) {
-    console.log('[ServiceWorker] Install');
     e.waitUntil(
         caches.open(cacheName).then(function (cache) {
-            console.log('[ServiceWorker] Caching app shell');
             return cache.addAll(['/']);
         })
     );
